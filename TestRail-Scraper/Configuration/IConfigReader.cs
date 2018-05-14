f43b91a5663149f6430843bf191b22c802d13887
Sophia Clarke <sophiaclarke93@gmail.com>
2018-05-14 16:26:50 +0100

    TestRail sdk, configreader

---
+++ TestRail-Scraper/Configuration/IConfigReader.cs
@@ -0,0 +1,47 @@
+﻿namespace TestRailScraper
+{
+
+    /// <summary>
+    /// A config reader is required to be supplied. This will probably vary by application so you will need to implement this.
+    /// </summary>
+    public interface IConfigReader
+    {
+        /// <summary>
+        /// Should return the API key required to connect to your team.
+        /// </summary>
+        string SlackApiKey { get; }
+
+        string TestRailUser { get; }
+        string TestRailPass { get; }
+
+        /// <summary>
+        /// Should the "help" middleware be used?
+        /// </summary>
+        bool HelpEnabled { get; }
+
+        /// <summary>
+        /// Should the "stats" middleware be used?
+        /// </summary>
+        bool StatsEnabled { get; }
+
+        /// <summary>
+        /// Should the "about" middleware be used?
+        /// </summary>
+        bool AboutEnabled { get; }
+
+        /// <summary>
+        /// Should the "Testrail" middleware be used?
+        /// </summary>
+        bool TestrailEnabled { get; }
+
+        /// <summary>
+        /// Should the "Hello" middleware be used?
+        /// </summary>
+        bool HelloEnabled { get; }
+
+        /// <summary>
+        /// Should return any other configuration values you need within your middleware/plugins.
+        /// </summary>
+        T GetConfigEntry<T>(string entryName);
+    }
+}
---
+++ TestRail-Scraper/Configuration/IConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// This dependency defines all the middleware/plugins you want to be executed in your application
+/// </summary>
+public interface IConfiguration
+{
+	/// <summary>
+	/// Priority order pipeline for middleware to be executed
+	/// </summary>
+	Type[] ListMiddlewareTypes();
+
+	/// <summary>
+	/// A list of plugins to use within the application
+	/// </summary>
+	Type[] ListPluginTypes();
+}
