60604666c00303d5b9765836b36d5140ad064fb1
Sophia Clarke <sophiaclarke93@gmail.com>
2018-05-14 15:17:06 +0100

    Empty VS project

---
+++ TestRail-Scraper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestRailScraper
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
